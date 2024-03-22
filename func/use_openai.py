e099f5230177f03407bb11a5d22aa6cb69a94f5f
rogerchang1108 <rogerchang1108@gmail.com>
2024-03-22 21:40:54 +0800

    feat: add exception handling in OpenAI API access

--- func/use_openai.py
+++ func/use_openai.py
@@ -8,15 +8,20 @@ def reply_from_openai(col, openai_api_key, model_selected, prompt_messages):
         api_key=openai_api_key,
         # api_key=os.environ.get('OPENAI_API_KEY'),
     )
-
-    # example token count from the OpenAI API
-    chat_completion = client.chat.completions.create(
-        model=model_selected,
-        messages=prompt_messages,
-        temperature=0,
-    )
-
-    col.write(f'{chat_completion.model}: model return by the OpenAI API.')
-    col.write(f'{chat_completion.usage.prompt_tokens} prompt(input) tokens counted by the OpenAI API.')
-    col.write(f'{chat_completion.usage.completion_tokens} completion(output) tokens counted by the OpenAI API.')
-    col.write(chat_completion.choices[0].message.content)
+    
+    try:
+        # example token count from the OpenAI API
+        chat_completion = client.chat.completions.create(
+            model=model_selected,
+            messages=prompt_messages,
+            temperature=0,
+        )
+        
+        col.write(f'{chat_completion.model}: model return by the OpenAI API.')
+        col.write(f'{chat_completion.usage.prompt_tokens} prompt(input) tokens counted by the OpenAI API.')
+        col.write(f'{chat_completion.usage.completion_tokens} completion(output) tokens counted by the OpenAI API.')
+        col.write(chat_completion.choices[0].message.content)
+    
+    except Exception as e:
+        col.info('❗Something went wrong! Check your OpenAI API Key!!!')
+        col.error(f'An error occurred: {e}')
