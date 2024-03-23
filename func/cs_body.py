1f4784c788d0dd922c95dd1bc44b80a4d8dba0f7
rogerchang1108 <rogerchang1108@gmail.com>
2024-03-23 17:56:29 +0800

    refactor: reindent code for improved readability

--- func/cs_body.py
+++ func/cs_body.py
@@ -55,17 +55,17 @@ def cs_body(example_messages):
         model_selected = st.selectbox(
             'Model',
             [
-            'gpt-3.5-turbo-0125', # GPT-3.5 Turbo
-            'gpt-3.5-turbo',
-            'gpt-3.5-turbo-1106', # Older models
-            'gpt-3.5-turbo-16k',
-            'gpt-3.5-turbo-0613', # Older models
-            'gpt-3.5-turbo-16k-0613', # Older models
-            'gpt-4-0125-preview', # GPT-4 Turbo
-            'gpt-4-turbo-preview',
-            'gpt-4-1106-preview', # GPT-4 Turbo
-            'gpt-4',
-            'gpt-4-0613', # Older models
+                'gpt-3.5-turbo-0125', # GPT-3.5 Turbo
+                'gpt-3.5-turbo',
+                'gpt-3.5-turbo-1106', # Older models
+                'gpt-3.5-turbo-16k',
+                'gpt-3.5-turbo-0613', # Older models
+                'gpt-3.5-turbo-16k-0613', # Older models
+                'gpt-4-0125-preview', # GPT-4 Turbo
+                'gpt-4-turbo-preview',
+                'gpt-4-1106-preview', # GPT-4 Turbo
+                'gpt-4',
+                'gpt-4-0613', # Older models
             ],
         )
 
