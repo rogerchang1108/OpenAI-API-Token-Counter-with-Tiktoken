0039f9f3190658e7da6eb7421c8e7798d6fa0369
rogerchang1108 <rogerchang1108@gmail.com>
2024-03-23 18:12:42 +0800

    feat: apply markdown-custom style and add unsupported models handling

--- func/use_tiktoken.py
+++ func/use_tiktoken.py
@@ -7,35 +7,41 @@ def num_tokens_from_messages(col, messages, model='gpt-3.5-turbo-0125'):
     try:
         encoding = tiktoken.encoding_for_model(model)
     except KeyError:
-        col.write('Warning: model not found. Using cl100k_base encoding.')
+        # col.write('Warning: model not found. Using cl100k_base encoding.')
         encoding = tiktoken.get_encoding('cl100k_base')
     if model in {
-        'gpt-4-0125-preview', # GPT-4 Turbo
-        'gpt-4-1106-preview', # GPT-4 Turbo
-        'gpt-4-0613', # Older models
-        'gpt-3.5-turbo-0125', # GPT-3.5 Turbo
-        'gpt-3.5-turbo-1106', # Older models
-        'gpt-3.5-turbo-0613', # Older models
-        'gpt-3.5-turbo-16k-0613', # Older models
+            'gpt-4-0125-preview', # GPT-4 Turbo
+            'gpt-4-1106-preview', # GPT-4 Turbo
+            'gpt-4-0613', # Older models
+            'gpt-3.5-turbo-0125', # GPT-3.5 Turbo
+            'gpt-3.5-turbo-1106', # Older models
+            'gpt-3.5-turbo-0613', # Older models
+            'gpt-3.5-turbo-16k-0613', # Older models
         }:
         tokens_per_message = 3
         tokens_per_name = 1
     elif 'gpt-4-turbo-preview' in model:
-        col.write('Warning: gpt-4-turbo-preview may update over time. Returning num tokens assuming gpt-4-0125-preview.')
+        col.markdown(f'<p class="markdown-custom-1">❗ Warning: gpt-4-turbo-preview may update over time. Returning num tokens assuming gpt-4-0125-preview.</p>', 
+                    unsafe_allow_html=True)
         return num_tokens_from_messages(col, messages, model='gpt-4-0125-preview')
     elif 'gpt-4' in model:
-        col.write('Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.')
+        col.markdown(f'<p class="markdown-custom-1">❗ Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.</p>', 
+                    unsafe_allow_html=True)
         return num_tokens_from_messages(col, messages, model='gpt-4-0613')
     elif 'gpt-3.5-turbo-16k' in model:
-        col.write('Warning: gpt-3.5-turbo-16k may update over time. Returning num tokens assuming gpt-3.5-turbo-16k-0613')
+        col.markdown(f'<p class="markdown-custom-1">❗ Warning: gpt-3.5-turbo-16k may update over time. Returning num tokens assuming gpt-3.5-turbo-16k-0613</p>', 
+                    unsafe_allow_html=True)
         return num_tokens_from_messages(col, messages, model='gpt-3.5-turbo-16k-0613')
     elif 'gpt-3.5-turbo' in model:
-        col.write('Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0125')
+        col.markdown(f'<p class="markdown-custom-1">❗ Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0125</p>', 
+                    unsafe_allow_html=True)
         return num_tokens_from_messages(col, messages, model='gpt-3.5-turbo-0125')
     else:
-        raise NotImplementedError(
-            f'''num_tokens_from_messages() is not implemented for model {model}.'''
-        )
+        col.error(f'Sorry, Token Counter is not implemented for model "{model}". Try another model!')
+        return 0
+        # raise NotImplementedError(
+        #     f'''num_tokens_from_messages() is not implemented for model {model}.'''
+        # )
     num_tokens = 0
     for message in messages:
         num_tokens += tokens_per_message
