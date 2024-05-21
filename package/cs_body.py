03a27f5ad3eab80a4daff103384989f501fc022a
rogerchang1108 <rogerchang1108@gmail.com>
2024-05-21 19:58:00 +0800

    feat: fetch open ai models dynamically

--- package/cs_body.py
+++ package/cs_body.py
@@ -97,23 +97,24 @@ def cs_body(new_example_messages):
                     st.session_state.message[i] = {'role': role, 'name': name, 'content': content}
                 else:
                     st.session_state.message[i] = {'role': role, 'content': content}
-
-            model_selected = st.selectbox(
-                'Model',
-                [
-                    'gpt-3.5-turbo-0125', # GPT-3.5 Turbo
-                    'gpt-3.5-turbo',
-                    'gpt-3.5-turbo-1106', # Older models
-                    'gpt-3.5-turbo-16k',
-                    'gpt-3.5-turbo-0613', # Older models
-                    'gpt-3.5-turbo-16k-0613', # Older models
-                    'gpt-4-0125-preview', # GPT-4 Turbo
-                    'gpt-4-turbo-preview',
-                    'gpt-4-1106-preview', # GPT-4 Turbo
-                    'gpt-4',
-                    'gpt-4-0613', # Older models
-                ],
-            )
+            
+            if openai_api_key:
+                try:
+                    client = OpenAI(api_key=openai_api_key)
+                    response = client.models.list()
+                    models = [model.id for model in response]
+                    models.sort()
+                    model_selected = st.selectbox(label = 'Model', 
+                                                  options = models, 
+                                                  index = len(models)//2)
+                except Exception as e:
+                    print(e)
+                    model_selected = st.selectbox(label = 'Model', 
+                                                  options =  ['gpt-4-turbo', ])
+                
+            else:
+                model_selected = st.selectbox(label = 'Model', 
+                                              options =  ['gpt-4-turbo', ])
             
             submit_button1 = st.form_submit_button(
                 label='Submit', 
